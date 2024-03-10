ae8ce275a007877e5dd11da5bc07eaf0d4edd572
maxwelldender <156535265+maxwelldender@users.noreply.github.com>
2024-03-10 16:03:26 -0400

    Update conf.py
    WEBSITE TITLE CHANGE

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../src/"))
 
 # -- Project information -----------------------------------------------------
 
-project = "Blank Project"
+project = "Blank Project-MAXWELL DENDER WEBSITE"
 copyright = "2024, John Doe"
 author = "John Doe"
 
